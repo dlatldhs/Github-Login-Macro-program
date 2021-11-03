13402b5eab8fb6b9ad99c374e8b7475672f02126
dlatldhs <80656700+dlatldhs@users.noreply.github.com>
2021-11-04 08:24:37 +0900

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from pynput.keyboard import Key, Controller
-
-bit=int(input("비트수를 입력해주세요:"))
-
-keyboard = Controller()
-
-# Press and release space
-keyboard.press(Key.space)
-keyboard.release(Key.space)
