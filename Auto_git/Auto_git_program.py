77861a69ca86e86b4e7974867628646af9c49691
dlatldhs <sionzz1713@gmail.com>
2021-11-03 13:38:02 +0900

    git 화면 연결

--- Auto_git/Auto_git_program.py
+++ Auto_git/Auto_git_program.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+import pyautogui as pag
 
+Name_or_email = input("What is your git name or email??:")
+PWD = input("What is your git password?:")
 
 #초기화
 driver = webdriver.Chrome('C:\\Users\\SW2126\\Desktop\\Python\\projects\\Macro_programs\\Auto_git\\chromedriver_win32\\chromedriver.exe')
-url = 'https://github.com/'
-
+url = 'https://github.com/'+Name_or_email
 driver.get(url) # url open
 driver.maximize_window() # 창 크게 만들기
 action = ActionChains(driver)
