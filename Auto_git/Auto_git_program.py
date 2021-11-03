eb77d820178707c03142de5b80c9245ffff27b26
dlatldhs <sionzz1713@gmail.com>
2021-11-03 22:09:32 +0900

    더 좋은 방식으로 수정 중

--- Auto_git/Auto_git_program.py
+++ Auto_git/Auto_git_program.py
@@ -4,24 +4,31 @@ from selenium.webdriver.common.action_chains import ActionChains
 import pyautogui as pagx
 import pyautogui
 import time
+import time as t
 
+
+#변수 설정
 name = input("What is your git name??:")
 email = input("What is your git email??:")
 pwd = input("What is your git password??:")
-
-#======실험=====실시간 마우스 좌표 구하기====== 나중에 필요할 때만 쓰는거
-'''
-while True:
-    print("Current Mouse Position:",pyautogui.position())
-    time.sleep(1)
-'''
-#========================================================================
 driver = webdriver.Chrome('C:\\Users\\SW2126\\Desktop\\Python\\projects\\Macro_programs\\Auto_git\\chromedriver_win32\\chromedriver.exe')
-url = 'https://github.com/login'
+url = 'https://google.com/'
 
 
+
+#url open & start!--
 driver.get(url) # url open
 driver.maximize_window() # 창 크게 만들기
+
+google_search_box = driver.find_element_by_css_selector('.gLFyf.gsfi')
+
+google_search_box.send_keys('github')
+google_search_box.send_keys(Keys.ENTER)
+driver.find_element_by_css_selector("LC20lb DKV0Md").Click()
+
+'''
+
+old version
 action = ActionChains(driver)
 time.sleep(3)
 #id 부분
@@ -32,3 +39,9 @@ pyautogui.click(938,464)
 #login 하기 부분
 pyautogui.typewrite(pwd, interval=0.1)
 pyautogui.click(938,581)
+
+url = 'https://github.com/'+name
+pyautogui.click(375,67)
+pyautogui.typewrite(url, interval=0.1)
+
+'''
